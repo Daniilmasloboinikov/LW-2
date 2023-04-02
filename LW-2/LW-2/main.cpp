4bbafa1996652ef65fb59b300bba20c7beae7886
Alice (MasloboinikovDA) <Daniilmasloboinikov@mail.ru>
2023-04-02 22:01:01 +0300

    max(a,b)

--- LW-2/LW-2/main.cpp
+++ LW-2/LW-2/main.cpp
@@ -17,5 +17,7 @@ int main(){
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    if (a>b) {cout << "max(a,b) = " << a << '\n';}
+    else {cout << "max(a,b) = " << b << '\n';}
     
 }
