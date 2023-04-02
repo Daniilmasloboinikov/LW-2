783075832d679476da96aa70bc4d766e237b9218
Daniilmasloboinikov <Daniilmasloboinikov@mail.ru>
2023-04-02 21:34:58 +0300

    code: заготовка

---
+++ LW-2/LW-2/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  LW-2
+//
+//  Created by MacBook on 02.04.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    
+}
